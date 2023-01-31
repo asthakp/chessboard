4b30761f2bc766e6f5c83e878e04d595f9da1711
Astha <aasudagreat@gmail.com>
2023-01-31 17:44:33 +0545

    players rotation

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 var heading=document.getElementById("player-heading")
 var container=document.getElementById("box")
 var rotation=0;
+var players=document.querySelectorAll(".fa-solid")
+console.log(players)
 
 
 heading.addEventListener("click", ()=>{
-    console.log(rotation)
     if (heading.innerText=="White"){
         rotation+=180;
         heading.innerText="Black"
         container.style.transform="rotate("+rotation+"deg)" 
+        
+        players.forEach(piece => {
+            piece.style.transform="rotate("+rotation+"deg)"
+        });
+    
         
     }
     else{
         rotation+=180;
         heading.innerText="White"
         container.style.transform="rotate("+rotation+"deg)"
+        
+        players.forEach(piece => {
+            piece.style.transform="rotate("+rotation+"deg)"
+        });
+        
         
     }
 
